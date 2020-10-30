9a071236cec2eb3265055040a5d227de5d5e389e
IvanCh20 <67267645+IvanCh20@users.noreply.github.com>
2020-10-30 18:39:52 +0300

    Update sql_task.sql

--- sql_task.sql
+++ sql_task.sql
@@ -7,3 +7,11 @@ WHERE
 GROUP BY
     department
 HAVING COUNT(*) < 5
+-- Ещё необходимо учесть отделы вообще без разработчиков
+UNION
+SELECT 
+    department
+FROM 
+    employees
+GROUP BY department, position
+NOT HAVING position = 'Software Developer'
